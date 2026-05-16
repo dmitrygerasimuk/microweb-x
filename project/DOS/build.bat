1aa07385b81849b07e49ae5fc81fbcd1fe6c9008
Dmitry <dmitrygerasimuk@gmail.com>
2026-05-17 01:14:19 +0300

    Use Watcom 1.9 DOS build environment

--- project/DOS/build.bat
+++ project/DOS/build.bat
@@ -1,7 +1,7 @@
-set PATH=c:\watcom\binnt;%PATH%
-set INCLUDE=c:\watcom\h
-set WATCOM=c:\watcom
-set EDPATH=c:\watcom\eddat
-set WIPFC=c:\watcom\wipfc
-set LIBDOS=c:\watcom\lib286\dos;c:\watcom\lib286
+set PATH=c:\watcom19\binnt;%PATH%
+set INCLUDE=c:\watcom19\h
+set WATCOM=c:\watcom19
+set EDPATH=c:\watcom19\eddat
+set WIPFC=c:\watcom19\wipfc
+set LIBDOS=c:\watcom19\lib286\dos;c:\watcom19\lib286
 wmake.exe
